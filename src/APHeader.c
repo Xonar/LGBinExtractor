b13eb093c31b743cb107707bd274e2fec01d6737
Paul le Roux <xonar.leroux@gmail.com>
2013-05-03 14:03:17 +0200

    Add Support for LG L9

--- src/APHeader.c
+++ src/APHeader.c
@@ -18,22 +18,22 @@ APHeader readAPHeader(FILE *f)
     fread(&out, sizeof(char) * 4, 1, f);
 
     /*PASS TO CORRECT FUNCTION BASED ON MAGIC NUMBER*/
-    switch(*magic)
+    switch (*magic)
     {
-    	case 0xaa55ec44:
-    		out = readAPHeader44EC55AA(f);
-    		break;
-        case 0xaa55a5a5:
-            out = readAPHeaderA5A555AA(f);
-            break;
-        case 0xaa55dd44:
-            out = readAPHeader44DD55AA(f);
-            break;
-        default:
-            fprintf(stderr, "Unsupported Bin Type : ");
-            printHex(stderr, magic, sizeof(*magic));
-            fprintf(stderr, "\n");
-            break;
+    case 0xaa55ec44:
+        out = readAPHeader44EC55AA(f);
+        break;
+    case 0xaa55a5a5:
+        out = readAPHeaderA5A555AA(f);
+        break;
+    case 0xaa55dd44:
+        out = readAPHeader44DD55AA(f);
+        break;
+    default:
+        fprintf(stderr, "Unsupported Bin Type : ");
+        printHex(stderr, magic, sizeof(*magic));
+        fprintf(stderr, "\n");
+        break;
     }
 
     return out;
@@ -41,48 +41,49 @@ APHeader readAPHeader(FILE *f)
 
 APHeader readAPHeader44EC55AA(FILE *f)
 {
-	APHeader out;
-	int i = 0;
-	
-	INIT_APHEADER(out);
-	
-	out.magic.magic=0xaa55ec44;
-	
-	/*COUNT AND ALLOCATE AP ENTRIES*/
-	while(1)
-	{
-	    uint64_t tmp;
-	    fread(&tmp, sizeof(tmp), 1, f);
-	    if(tmp == UINT64_MAX) break;
-	    else out.pent_num++;
-	}
-	
-	fseek(f, 4, SEEK_SET);
+    APHeader out;
+    int i = 0;
 
-	out.pent_arr = (APPartitionEntry*) calloc(out.pent_num, sizeof(APPartitionEntry));
+    INIT_APHEADER(out);
 
-	/*READ FILE OFFSET AND FILE SIZE*/
-	for(; i < out.pent_num; i++)
-	{
-	    fread(&out.pent_arr[i].file_off, sizeof(uint32_t), 1, f);
-	    out.pent_arr[i].disk_off = out.pent_arr[i].file_off;
-	    fread(&out.pent_arr[i].file_size, sizeof(uint32_t), 1, f);
-	}
-	
-	/*READ ID, DISK SIZE AND NAME*/
-	fseek(f, 0x200,SEEK_SET);
-	
-	for(i = 0 ; i < out.pent_num; i++)
-	{
-		 fread(&out.pent_arr[i].pent_id,sizeof(uint32_t),1,f);
-		 fread(&out.pent_arr[i].disk_size, sizeof(uint32_t), 1, f);
-		 fseek(f, 0x4, SEEK_CUR);
-		 fread(&out.pent_arr[i].name, sizeof(char), 0x14, f);
-		 out.pent_arr[i].name[0x14]='\0';
-		 fseek(f, 0x1e0, SEEK_CUR);
-	}
-	
-	return out;
+    out.magic.magic = 0xaa55ec44;
+
+    /*COUNT AND ALLOCATE AP ENTRIES*/
+    while (1)
+    {
+        uint64_t tmp;
+        fread(&tmp, sizeof(tmp), 1, f);
+        if (tmp == UINT64_MAX) break;
+        else out.pent_num++;
+    }
+
+    fseek(f, 4, SEEK_SET);
+
+    out.pent_arr = (APPartitionEntry*) calloc(out.pent_num,
+            sizeof(APPartitionEntry));
+
+    /*READ FILE OFFSET AND FILE SIZE*/
+    for (; i < out.pent_num; i++)
+    {
+        fread(&out.pent_arr[i].file_off, sizeof(uint32_t), 1, f);
+        out.pent_arr[i].disk_off = out.pent_arr[i].file_off;
+        fread(&out.pent_arr[i].file_size, sizeof(uint32_t), 1, f);
+    }
+
+    /*READ ID, DISK SIZE AND NAME*/
+    fseek(f, 0x200, SEEK_SET);
+
+    for (i = 0; i < out.pent_num; i++)
+    {
+        fread(&out.pent_arr[i].pent_id, sizeof(uint32_t), 1, f);
+        fread(&out.pent_arr[i].disk_size, sizeof(uint32_t), 1, f);
+        fseek(f, 0x4, SEEK_CUR);
+        fread(&out.pent_arr[i].name, sizeof(char), 0x14, f);
+        out.pent_arr[i].name[0x14] = '\0';
+        fseek(f, 0x1e0, SEEK_CUR);
+    }
+
+    return out;
 }
 
 APHeader readAPHeaderA5A555AA(FILE *f)
@@ -92,23 +93,24 @@ APHeader readAPHeaderA5A555AA(FILE *f)
 
     INIT_APHEADER(out);
 
-    out.magic.magic=0xaa55a5a5;
+    out.magic.magic = 0xaa55a5a5;
 
     /*COUNT AND ALLOCATE AP ENTRIES*/
-    while(1)
+    while (1)
     {
         uint64_t tmp;
         fread(&tmp, sizeof(tmp), 1, f);
-        if(tmp == UINT64_MAX) break;
+        if (tmp == UINT64_MAX) break;
         else out.pent_num++;
     }
 
     fseek(f, 4, SEEK_SET);
 
-    out.pent_arr = (APPartitionEntry*) calloc(out.pent_num, sizeof(APPartitionEntry));
+    out.pent_arr = (APPartitionEntry*) calloc(out.pent_num,
+            sizeof(APPartitionEntry));
 
     /*READ FILE OFFSET AND SIZE*/
-    for(; i < out.pent_num; i++)
+    for (; i < out.pent_num; i++)
     {
         fread(&out.pent_arr[i].file_off, sizeof(uint32_t), 1, f);
         fread(&out.pent_arr[i].file_size, sizeof(uint32_t), 1, f);
@@ -117,7 +119,7 @@ APHeader readAPHeaderA5A555AA(FILE *f)
     skipToNextLBA(f);
 
     /*READ DISK SIZE IGNORING FIRST ID REFERENCE*/
-    for(i = 0; i < out.pent_num; i++)
+    for (i = 0; i < out.pent_num; i++)
     {
         /*fread(&out.pent_arr[i].pent_id,sizeof(uint32_t),1,f);*/
         fseek(f, 4, SEEK_CUR);
@@ -128,11 +130,11 @@ APHeader readAPHeaderA5A555AA(FILE *f)
     fseek(f, 0x2200, SEEK_SET);
 
     /*READ ID AND NAME AND SET DISK OFF*/
-    for(i = 0; i < out.pent_num; i++)
+    for (i = 0; i < out.pent_num; i++)
     {
         fread(&out.pent_arr[i].pent_id, sizeof(uint32_t), 1, f);
         fread(&out.pent_arr[i].name, sizeof(char), 256, f);
-        out.pent_arr[i].name[256]='\0';
+        out.pent_arr[i].name[256] = '\0';
         out.pent_arr[i].disk_off = 0xFFFFFFFF;
     }
 
@@ -158,22 +160,24 @@ APHeader readAPHeader44DD55AA(FILE *f)
     fread(&curMagicNum->magic, sizeof(uint32_t), 1, f);
 
     /*SCAN FOR MAGIC NUMBERS*/
-    for(; i < 0x2004; i += 4)
+    for (; i < 0x2004; i += 4)
     {
         fread(&tmp, sizeof(uint32_t), 1, f);
 
-        if(tmp != 0xffffffff)
+        if (tmp != 0xffffffff)
         {
+            fprintf(stderr, "FOUND MAGIC NUM %x @ %x!\n", tmp, i);
             /*FOUND MAGIC NUM*/
             curMagicNum->next = malloc(sizeof(MagicNumber));
             curMagicNum = curMagicNum->next;
+            curMagicNum->next = NULL;
             curMagicNum->magic = tmp;
             curMagicNum->off = i;
         }
     }
 
     /*START WITH 44 DD 55 AA and ENDING WITH 33 EC 55 AA*/
-    if(out.magic.magic != 0xaa55dd44 || curMagicNum->magic != 0xaa55ec33)
+    if (out.magic.magic != 0xaa55dd44 || curMagicNum->magic != 0xaa55ec33)
     {
         fprintf(stderr, "Not 0x0    :44DD55AA\n    0x2000 :33EC55AA\n");
         goto failed;
@@ -201,8 +205,9 @@ APHeader readAPHeader44DD55AA(FILE *f)
     curDataBlock = &dataBlock;
 
     /*TOT FILES*/
-    if(out.magic.next->off == 0x8 && out.magic.next->next->off == 0x2000) switch(out.magic.next->magic)
-    {
+    if (out.magic.next->off == 0x8 && out.magic.next->next->off == 0x2000)
+        switch (out.magic.next->magic)
+        {
         default:
             printf("Did not detect known magic numbers!\n"
                     "  The File Appears to be a TOT File.All known TOT Files\n"
@@ -211,7 +216,7 @@ APHeader readAPHeader44DD55AA(FILE *f)
 
             scanf("%s", buf);
 
-            if(!(buf[0] == 'Y' || buf[0] == 'y')) break;
+            if (!(buf[0] == 'Y' || buf[0] == 'y')) break;
 
         case 0x8978f62b:
         case 0x5062c8ea:
@@ -219,7 +224,7 @@ APHeader readAPHeader44DD55AA(FILE *f)
         case 0xdebf33af:
         case 0x42ef4e39:
         case 0x0e65f034:
-        	/*tot*/
+            /*tot*/
             curDataBlock->next = malloc(sizeof(DataBlock));
             curDataBlock = curDataBlock->next;
             curDataBlock->blockOff = 0x4220;
@@ -231,77 +236,91 @@ APHeader readAPHeader44DD55AA(FILE *f)
             curDataBlock->items[0].size = 0x20;
 
             goto readBlocks;
-    }
-
-    if(out.magic.next->off == 0x600 && out.magic.next->next->off == 0x2000)
-        switch(out.magic.next->magic)
-        {
-            case 0xcc00bbaa:
-                curDataBlock->next = malloc(sizeof(DataBlock));
-                curDataBlock = curDataBlock->next;
-                curDataBlock->blockOff = 0x2400;
-                curDataBlock->blockSize = 0x20;
-
-                curDataBlock->numItems = 5;
-                curDataBlock->items = calloc(sizeof(Item), 5);
-                curDataBlock->items[0].type = BLOCK_ID;
-                curDataBlock->items[0].size = 4;
-                curDataBlock->items[1].type = DISK_SIZE;
-                curDataBlock->items[1].size = 4;
-                curDataBlock->items[2].type = SKIP;
-                curDataBlock->items[2].size = 4;
-                curDataBlock->items[3].type = BLOCK_NAME;
-                curDataBlock->items[3].size = 0x14;
-                curDataBlock->items[4].type = SKIP;
-                curDataBlock->items[4].size = 0x1E0;
-
-                goto readBlocks;
         }
 
-    if(out.magic.next->off == 0x2000)
+    if (out.magic.next->off == 0x600 && out.magic.next->next->off == 0x2000)
+        switch (out.magic.next->magic)
+        {
+        case 0xcc00bbaa:
+            curDataBlock->next = malloc(sizeof(DataBlock));
+            curDataBlock = curDataBlock->next;
+            curDataBlock->blockOff = 0x2400;
+            curDataBlock->blockSize = 0x20;
+
+            curDataBlock->numItems = 5;
+            curDataBlock->items = calloc(sizeof(Item), 5);
+            curDataBlock->items[0].type = BLOCK_ID;
+            curDataBlock->items[0].size = 4;
+            curDataBlock->items[1].type = DISK_SIZE;
+            curDataBlock->items[1].size = 4;
+            curDataBlock->items[2].type = SKIP;
+            curDataBlock->items[2].size = 4;
+            curDataBlock->items[3].type = BLOCK_NAME;
+            curDataBlock->items[3].size = 0x14;
+            curDataBlock->items[4].type = SKIP;
+            curDataBlock->items[4].size = 0x1E0;
+
+            goto readBlocks;
+        }
+
+    if (out.magic.next->off == 0x2000)
     {
-    	/*CHECK FOR THIRD MAGIC NUM AT 0x4000*/
-    	fseek(f, 0x4000, SEEK_SET);
-    	fread(&tmp, sizeof(uint32_t), 1, f);
-    	
-    	if(tmp!=0xffffffff)
-    	{
-			/*FOUND MAGIC NUM*/
-			curMagicNum->next = malloc(sizeof(MagicNumber));
-			curMagicNum = curMagicNum->next;
-			curMagicNum->magic = tmp;
-			curMagicNum->off = 0x4000;
-			
-			switch(curMagicNum->magic)
-			{
-			case 0x56781234:
-				curDataBlock->next = malloc(sizeof(DataBlock));
-				curDataBlock = curDataBlock->next;
-				curDataBlock->blockOff = 0x4220;
-				curDataBlock->blockSize = 0x20;
-				    		
-				curDataBlock->numItems = 1;
-				curDataBlock->items = calloc(sizeof(Item), 1);
-				curDataBlock->items[0].type = BLOCK_NAME;
-				curDataBlock->items[0].size = 0x20;
-				break;
-			default:
-				goto failed;
-			}
-    	}
-    	else
-    	{
-			curDataBlock->next = malloc(sizeof(DataBlock));
-			curDataBlock = curDataBlock->next;
-			curDataBlock->blockOff = 0x2404;
-			curDataBlock->blockSize = 0x14;
-    		
-			curDataBlock->numItems = 1;
-			curDataBlock->items = calloc(sizeof(Item), 1);
-			curDataBlock->items[0].type = BLOCK_NAME;
-			curDataBlock->items[0].size = 0x14;
-    	}
-    	
+        printf("%x %x\n", out.magic.next->magic, out.magic.next->off);
+        /*CHECK FOR THIRD MAGIC NUM AT 0x4000*/
+        fseek(f, 0x4000, SEEK_SET);
+        fread(&tmp, sizeof(uint32_t), 1, f);
+
+        if (tmp == 0x00000000)
+        {
+            curDataBlock->next = malloc(sizeof(DataBlock));
+            curDataBlock = curDataBlock->next;
+            curDataBlock->blockOff = 0x3004;
+            curDataBlock->blockSize = 0x14;
+
+            curDataBlock->numItems = 1;
+            curDataBlock->items = calloc(sizeof(Item), 1);
+            curDataBlock->items[0].type = BLOCK_NAME;
+            curDataBlock->items[0].size = 0x14;
+        }
+        else if (tmp != 0xffffffff)
+        {
+            /*FOUND MAGIC NUM*/
+            curMagicNum = out.magic.next;
+            curMagicNum->next = malloc(sizeof(MagicNumber));
+            curMagicNum = curMagicNum->next;
+            curMagicNum->magic = tmp;
+            curMagicNum->off = 0x4000;
+
+            switch (curMagicNum->magic)
+            {
+            case 0x56781234:
+                curDataBlock->next = malloc(sizeof(DataBlock));
+                curDataBlock = curDataBlock->next;
+                curDataBlock->blockOff = 0x4220;
+                curDataBlock->blockSize = 0x20;
+
+                curDataBlock->numItems = 1;
+                curDataBlock->items = calloc(sizeof(Item), 1);
+                curDataBlock->items[0].type = BLOCK_NAME;
+                curDataBlock->items[0].size = 0x20;
+                break;
+            default:
+                goto failed;
+            }
+        }
+        else
+        {
+            curDataBlock->next = malloc(sizeof(DataBlock));
+            curDataBlock = curDataBlock->next;
+            curDataBlock->blockOff = 0x2404;
+            curDataBlock->blockSize = 0x14;
+
+            curDataBlock->numItems = 1;
+            curDataBlock->items = calloc(sizeof(Item), 1);
+            curDataBlock->items[0].type = BLOCK_NAME;
+            curDataBlock->items[0].size = 0x14;
+        }
+
         goto readBlocks;
     }
 
@@ -315,16 +334,17 @@ APHeader readAPHeader44DD55AA(FILE *f)
 
     fread(buf, sizeof(char), 0x10, f);
 
-    while(*((uint32_t*) buf) != 0xFFFFFFFF)
+    while (*((uint32_t*) buf) != 0xFFFFFFFF)
     {
         out.pent_num++;
         fread(buf, sizeof(uint32_t), 4, f);
     }
 
-    out.pent_arr = (APPartitionEntry*) calloc(out.pent_num, sizeof(APPartitionEntry));
+    out.pent_arr = (APPartitionEntry*) calloc(out.pent_num,
+            sizeof(APPartitionEntry));
 
     /*INIT AP PARTITION ENTRIES*/
-    for(i = 0; i < out.pent_num; i++)
+    for (i = 0; i < out.pent_num; i++)
     {
         out.pent_arr[i].disk_off = 0xffffffff;
         out.pent_arr[i].file_off = 0xffffffff;
@@ -337,38 +357,44 @@ APHeader readAPHeader44DD55AA(FILE *f)
     /*READ APDATA*/
     curDataBlock = &dataBlock;
 
-    while(curDataBlock != NULL )
+    while (curDataBlock != NULL)
     {
         fseek(f, curDataBlock->blockOff, SEEK_SET);
 
-        for(i = 0; i < out.pent_num; i++)
+        for (i = 0; i < out.pent_num; i++)
         {
-            for(j = 0; j < curDataBlock->numItems; j++)
+            for (j = 0; j < curDataBlock->numItems; j++)
             {
-                switch(curDataBlock->items[j].type)
+                switch (curDataBlock->items[j].type)
                 {
-                    case DISK_SIZE:
-                        fread(&out.pent_arr[i].disk_size, curDataBlock->items[j].size, 1, f);
-                        break;
-                    case DISK_OFF:
-                        fread(&out.pent_arr[i].disk_off, curDataBlock->items[j].size, 1, f);
-                        break;
-                    case FILE_SIZE:
-                        fread(&out.pent_arr[i].file_size, curDataBlock->items[j].size, 1, f);
-                        break;
-                    case FILE_OFF:
-                        fread(&out.pent_arr[i].file_off, curDataBlock->items[j].size, 1, f);
-                        break;
-                    case BLOCK_ID:
-                        fread(&out.pent_arr[i].pent_id, curDataBlock->items[j].size, 1, f);
-                        break;
-                    case BLOCK_NAME:
-                        fread(&out.pent_arr[i].name, sizeof(char), curDataBlock->items[j].size, f);
-                        out.pent_arr[i].name[curDataBlock->items[j].size]='\0';
-                        break;
-                    case SKIP:
-                        fseek(f, curDataBlock->items[j].size, SEEK_CUR);
-                        break;
+                case DISK_SIZE:
+                    fread(&out.pent_arr[i].disk_size,
+                            curDataBlock->items[j].size, 1, f);
+                    break;
+                case DISK_OFF:
+                    fread(&out.pent_arr[i].disk_off,
+                            curDataBlock->items[j].size, 1, f);
+                    break;
+                case FILE_SIZE:
+                    fread(&out.pent_arr[i].file_size,
+                            curDataBlock->items[j].size, 1, f);
+                    break;
+                case FILE_OFF:
+                    fread(&out.pent_arr[i].file_off,
+                            curDataBlock->items[j].size, 1, f);
+                    break;
+                case BLOCK_ID:
+                    fread(&out.pent_arr[i].pent_id, curDataBlock->items[j].size,
+                            1, f);
+                    break;
+                case BLOCK_NAME:
+                    fread(&out.pent_arr[i].name, sizeof(char),
+                            curDataBlock->items[j].size, f);
+                    out.pent_arr[i].name[curDataBlock->items[j].size] = '\0';
+                    break;
+                case SKIP:
+                    fseek(f, curDataBlock->items[j].size, SEEK_CUR);
+                    break;
                 }
             }
         }
@@ -377,8 +403,8 @@ APHeader readAPHeader44DD55AA(FILE *f)
     }
 
     /*MAKE SURE ID IS SET*/
-    for(i = 0; i < out.pent_num; i++)
-        if(out.pent_arr[i].pent_id == 0xffffffff) out.pent_arr[i].pent_id = i;
+    for (i = 0; i < out.pent_num; i++)
+        if (out.pent_arr[i].pent_id == 0xffffffff) out.pent_arr[i].pent_id = i;
 
     return out;
     failed: fprintf(stderr, "Failed Reading APHeader!\n");
@@ -389,13 +415,14 @@ void printAPHeader(const APHeader h, FILE *f)
 {
     /*PRINT MAGIC NUMBER(S)*/
     MagicNumber* tmp = h.magic.next;
-    fprintf(f, "AP HEADER\n----------\n%-30s0x%-8X: ", "Magic Number",h.magic.off);
+    fprintf(f, "AP HEADER\n----------\n%-30s0x%-8X: ", "Magic Number",
+            h.magic.off);
 
     printHex(f, &h.magic.magic, 4);
 
-    while(tmp)
+    while (tmp)
     {
-        fprintf(f, "\n%-30s0x%-8X: ", "",tmp->off);
+        fprintf(f, "\n%-30s0x%-8X: ", "", tmp->off);
         printHex(f, &tmp->magic, 4);
         tmp = tmp->next;
     }
@@ -412,21 +439,21 @@ void printAPPartitionEntry(const APPartitionEntry pe, FILE *f)
             "    %-26s%" PRIu32 "\n",
             "Data Block Name", pe.name, "Data Block ID", pe.pent_id,
             "Size on File", pe.file_size);
-    
-    if(pe.file_off != 0xFFFFFFFF)
-    {
-            fprintf(f, "    %-26s%" PRIu32 "\n", "File Offset", pe.file_off);
-    }
 
-    if(pe.disk_size != 0xFFFFFFFF)
+    if (pe.file_off != 0xFFFFFFFF)
     {
-        fprintf(f, "    %-26s%" PRIu32 "\n", "Size on Disk", pe.disk_size);
-    }
+fprintf    (f, "    %-26s%" PRIu32 "\n", "File Offset", pe.file_off);
+}
 
-    if(pe.disk_off != 0xFFFFFFFF)
+    if (pe.disk_size != 0xFFFFFFFF)
     {
-        fprintf(f, "    %-26s%" PRIu32 "\n", "Disk Offset", pe.disk_off);
-    }
+fprintf    (f, "    %-26s%" PRIu32 "\n", "Size on Disk", pe.disk_size);
+}
+
+    if (pe.disk_off != 0xFFFFFFFF)
+    {
+fprintf    (f, "    %-26s%" PRIu32 "\n", "Disk Offset", pe.disk_off);
+}
 }
 
 void printFullAPInfo(const APHeader h, FILE *f)
@@ -437,7 +464,7 @@ void printFullAPInfo(const APHeader h, FILE *f)
 
     fprintf(f, "\nPARTITION ENTRIES\n-----------------\n");
 
-    for(; i < h.pent_num; i++)
+    for (; i < h.pent_num; i++)
     {
         fputs("\n", f);
         printAPPartitionEntry(h.pent_arr[i], f);
