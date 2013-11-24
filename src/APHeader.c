0e6e1e128a4a5fd7ed8df7782dc44dc308b2b82a
tkawajir <taka.kawajiri@gmail.com>
2013-11-25 06:45:55 +0900

    Add Support Optimus G Pro(E9890)

--- src/APHeader.c
+++ src/APHeader.c
@@ -254,6 +254,7 @@ APHeader readAPHeader44DD55AA(FILE *f)
     case 0x0e65f034:
     case 0x95f57d8c:
     case 0x729092c9:
+    case 0x71b31218:
       /*tot*/
       curDataBlock->next = malloc(sizeof(DataBlock));
       curDataBlock = curDataBlock->next;
