2242cbd4762518b02fccbcc8f978635d7a4601ea
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-11 15:45:25 -0500

    Add support for LS990 ZV4 (Sprint G3)

--- src/APHeader.c
+++ src/APHeader.c
@@ -309,6 +309,7 @@ APHeader readAPHeader44DD55AA(FILE *f)
     case 0xf282d8eb:
     case 0xc4c4055a: /* LG G2-Mini (D620) */
     case 0x191c1a47: /* Verizon G3 (VS985 10B) */
+    case 0x1d3b326d: /* Sprint G3 (LS990 ZV4) */
       /* Nexus 5 tot*/
       curDataBlock->next = malloc(sizeof(DataBlock));
       curDataBlock = curDataBlock->next;
